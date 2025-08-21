32a3c67392c56113f3afc3dd58b1752b3f356064
Fedor Partanskiy <fredprtnsk@gmail.com>
2025-08-21 07:13:17 +0300

    run test with pkcs11
    Signed-off-by: Fedor Partanskiy <fredprtnsk@gmail.com>

--- scripts/run-unit-tests.sh
+++ scripts/run-unit-tests.sh
@@ -6,4 +6,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-time go test -race -cover ./...
+time {
+    go test -race -cover ./...
+    go test -race -cover -tags "pkcs11" github.com/hyperledger/fabric-lib-go/bccsp/factory
+    go test -race -cover -tags "pkcs11" github.com/hyperledger/fabric-lib-go/bccsp/pkcs11
+}
